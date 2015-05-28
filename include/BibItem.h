d5cfbac1803d44b1041b7169931e6e1a6c535fe3
Norival <xavier.laviron@gmx.fr>
2015-05-28 18:38:58 +0200

    Adding class BibItem

---
+++ include/BibItem.h
@@ -0,0 +1,19 @@
+#ifndef BIBITEM_H
+#define BIBITEM_H
+
+class BibItem
+{
+  public:
+
+
+  protected:
+    std::string m_author;
+    std::string m_title;
+    std::string m_label;
+
+};
+
+
+
+
+#endif
