eef5a7db39dd3ec9662cd770f35b67bb1b32804f
Norival <xavier.laviron@gmx.fr>
2015-06-02 22:53:05 +0200

    Ajout de la classe BibFile

---
+++ include/BibFile.h
@@ -0,0 +1,27 @@
+#ifndef BIBFILE_H
+#define BIBFILE_H
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+
+
+class BibFile
+{
+  public:
+
+
+  protected:
+
+
+};
+
+
+
+
+
+
+
+
+#endif
---
+++ src/BibFile.cpp
@@ -0,0 +1,3 @@
+#include "BibFile.h"
+
+using namespace std;
