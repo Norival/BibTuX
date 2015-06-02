9bbb7efdeb0e3f767eecf7ca64c9355129d01afd
Norival <xavier.laviron@gmx.fr>
2015-06-03 01:00:29 +0200

    Début implémentation classe BibFile

--- include/BibFile.h
+++ include/BibFile.h
@@ -10,9 +10,13 @@
 class BibFile
 {
   public:
+    BibFile(std::string bibpath);
+    void readBib(const std::string &bibpath, std::vector<std::string> &file);
 
 
   protected:
+    std::string m_bibPath;
+    std::vector<std::string> m_File;
 
 
 };
--- src/BibFile.cpp
+++ src/BibFile.cpp
@@ -1,3 +1,57 @@
 #include "BibFile.h"
+#include <BibArticle.h>
+#include <Config.h>
 
 using namespace std;
+
+
+BibFile::BibFile(string bibpath):
+  m_bibPath(bibpath)
+{
+  //Constructor
+}
+
+BibFile::readBib(const string &bibpath, vector<string> &file)
+{
+  ifstream bibfile(bibpath.c_str());
+  
+  if (bibfile)
+  {
+    //Création de variables temporaires pour stocker les données
+    string line;
+
+    string keyTmp = "";
+    string authorTmp = "";
+    string journalTmp = "";
+    string pagesTmp = "";
+    string monthTmp = "";
+    int yearTmp = "";
+    int titleTmp = "";
+    int volumeTmp = "";
+    int numberTmp = "";
+
+    while (getline(bibfile, line))
+    {
+      if (line[0] != '@')
+        continue;
+      else if (line.substr(1, 7) == "Article")
+      {
+        keyTmp = line.substr(9, line.size()-1)
+        while (getline(bibfile, line))
+        {
+          if (line[0] != '}')
+          {
+            for (int i = 0; i < line.size(), i++)
+            {
+              switch (line[i])
+              {
+                case 'a' : //Author
+                  if (
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
