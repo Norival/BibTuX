2763dbc9fbc19e33da2f15cd34287b3d3c4a224e
Norival <xavier.laviron@gmx.fr>
2015-06-02 22:48:33 +0200

    Implémentation de la classe BibArticle

--- include/BibArticle.h
+++ include/BibArticle.h
@@ -5,16 +5,39 @@
 #include <string>
 #include <vector>
 
+#include "BibItem.h"
+
+struct characteristics
+{
+  std::string Author;
+  std::string Journal;
+  std::string Pages;
+  std::string Month;
+  int Year;
+  int Title;
+  int Volume;
+  int Number;
+};
+
 class BibArticle : public BibItem
 {
   public:
-    BibArticle(std::string m_key);
+    BibArticle();
+    BibArticle(std::string key,
+        std::string author,
+        std::string journal,
+        std::string pages,
+        std::string month,
+        int year,
+        int title,
+        int volume,
+        int number
+        );
+    characteristics getCharacteristics() const;
 
   protected:
+    characteristics m_Char;
 
 };
-
-
-
 
 #endif
--- src/BibArticle.cpp
+++ src/BibArticle.cpp
@@ -1,3 +1,37 @@
 #include "BibArticle.h"
 
 using namespace std;
+
+
+BibArticle::BibArticle() : BibItem()
+{
+  //Constructor
+}
+
+BibArticle::BibArticle(string key,
+    string author,
+    string journal,
+    string pages,
+    string month,
+    int year,
+    int title,
+    int volume,
+    int number
+    )
+{
+  m_Char.Author= author;
+  m_Char.Journal = journal;
+  m_Char.Pages = pages;
+  m_Char.Month = month;
+
+  m_Char.Year = year;
+  m_Char.Title = title;
+  m_Char.Volume = volume;
+  m_Char.Number = number;
+}
+
+characteristics BibArticle::getCharacteristics(void) const
+{
+  return m_Char;
+}
+
