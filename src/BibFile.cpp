284aa9dafd789632836ad411500b8e9bdbd6d273
Norival <xavier.laviron@gmx.fr>
2015-06-03 22:54:59 +0200

    Test implémentation classe BibFile

--- src/BibFile.cpp
+++ src/BibFile.cpp
@@ -19,59 +19,98 @@ void BibFile::readBib(const string &bibpath)//, vector<string> &file)
   {
     //Création de variables temporaires pour stocker les données
     string line;
-    vector<string> items(0);
-    string itemTmp;
+    //vector<string> items(0);
+    //string itemTmp;
+    string type;
 
     string keyTmp = "";
     string authorTmp = "";
     string journalTmp = "";
     string pagesTmp = "";
     string monthTmp = "";
+    string titleTmp = "";
     int yearTmp = 0;
-    int titleTmp = 0;
     int volumeTmp = 0;
     int numberTmp = 0;
 
     while (getline(bibfile, line))
     {
-      //Enregistrement des items dans des strings
-      if (line[0] != '@')
+      //Boucle générale, lecture du fichier
+      if (line.rfind('@') == line.npos) //Pas de @
         continue;
-      else
+      else //Présence d'un @
       {
-        itemTmp = "";
-        while (line[0] != '}')
+        keyTmp = line.substr(
+            line.rfind('{') + 1,
+            line.size() - 1);
+        if (line.rfind("Article") != line.npos) //C'est un article
+          type = "article";
+
+        while (getline(bibfile, line))
         {
-          getline(bibfile, line);
-          itemTmp += line;
+          //Lecture des composantes et sockage dans les strings Tmp
+          if (line.rfind("author") != line.npos)
+          {
+            authorTmp = line.substr(
+                line.rfind('{') + 1,
+                line.rfind('}') - 1);
+          }
+          if (line.rfind("journal") != line.npos)
+          {
+            journalTmp = line.substr(
+                line.rfind('{') + 1,
+                line.rfind('}') - 1);
+          }
+          if (line.rfind("pages") != line.npos)
+          {
+            pagesTmp = line.substr(
+                line.rfind('{') + 1,
+                line.rfind('}') - 1);
+          }
+          if (line.rfind("month") != line.npos)
+          {
+            monthTmp = line.substr(
+                line.rfind('{') + 1,
+                line.rfind('}') - 1);
+          }
+          if (line.rfind("title") != line.npos)
+          {
+            titleTmp = line.substr(
+                line.rfind('{') + 1,
+                line.rfind('}') - 1);
+          }
+          
+          if (line.rfind("year") != line.npos)
+          {
+            string str;
+            str = line.substr(
+                line.rfind('{') + 1,
+                line.rfind('}') - 1);
+            Config::fromString(str, yearTmp);
+          }
+          if (line.rfind("volume") != line.npos)
+          {
+            string str;
+            str = line.substr(
+                line.rfind('{') + 1,
+                line.rfind('}') - 1);
+            Config::fromString(str, volumeTmp);
+          }
+          if (line.rfind("number") != line.npos)
+          {
+            string str;
+            str = line.substr(
+                line.rfind('{') + 1,
+                line.rfind('}') - 1);
+            Config::fromString(str, numberTmp);
+          }
         }
-        items.push_back(itemTmp);
       }
     }
 
-  }
-
-  for (int i = 0; i < items.size(); i++)
-  {
-    //Lecture de tous les items
-    itemTmp = items[i];
-    type = itemTmp.substr(1, 4);
-    int j = 0;
-    while (j < itemTmp.size())
-    {
-      //Récupération de key dans keyTmp
-      if (itemTmp[j] == '{')
-      {
-        while (itemTmp[j+1] != ',')
-        {
-          keyTmp += itemTmp[j+1];
-          j++;
-        }
-      }
-      //Récupération de author dans authorTmp
-
-    }
   }
   else
     cout << "Unable to read Bibtex file..." << endl;
 }
+
+template<typename T>
